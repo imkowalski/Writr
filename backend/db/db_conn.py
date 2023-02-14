cb5de4cdebf6eb289016ea4718ff981bac22899f
Michal Kowalski <kowal@outlook.dk>
2023-02-14 13:19:52 +0100

    db_conn edited

--- backend/db/db_conn.py
+++ backend/db/db_conn.py
@@ -1,8 +1,7 @@
 import redis
 
 # Connect to Redis using redis-py
-
 conn = redis.Redis(
-host='redis-15770.c55.eu-central-1-1.ec2.cloud.redislabs.com',
-port=15770,
-password='4UXkv5AdRfzrAC1RSvZv8iXDNFCVjziw')
+  host='redis-16138.c55.eu-central-1-1.ec2.cloud.redislabs.com',
+  port=16138,
+  password='45mrNGXahNsovQrqmMk8YZPI2tAjA5tI')
