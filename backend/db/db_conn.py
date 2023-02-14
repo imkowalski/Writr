b11aeb7eb267479ceb8ea77e52b01b8cf18c1f9d
Michal <kowal@outlook.dk>
2023-02-14 13:18:04 +0100

    Delete db_conn.py

--- backend/db/db_conn.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-# Connect to Redis using redis-py
-
-conn = redis.Redis(
-host='redis-15770.c55.eu-central-1-1.ec2.cloud.redislabs.com',
-port=15770,
-password='4UXkv5AdRfzrAC1RSvZv8iXDNFCVjziw')
